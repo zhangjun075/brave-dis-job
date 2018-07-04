7c70c2570158f3642a338c0c528afbb6ebef08ab
Jun Zhang <zhangjunheling@163.com>
2018-07-04 10:23:15 +0800

    updated by zj

--- src/main/java/com/brave/annotation/JobNameProcessor.java
+++ src/main/java/com/brave/annotation/JobNameProcessor.java
@@ -37,22 +37,6 @@ public class JobNameProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName)
         throws BeansException {
-//        Field[] fields = bean.getClass().getFields();
-//        if (fields != null) {
-//            for (Field field : fields) {
-//                JobName myValue = AnnotationUtils.findAnnotation(field, JobName.class);
-//                if (myValue != null) {
-//                    String name = myValue.name();
-//                    field.setAccessible(true);
-//                    try {
-//                        field.set(bean, name);
-//                        field.setAccessible(false);
-//                    } catch (IllegalAccessException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//        }
         return bean;
     }
 
