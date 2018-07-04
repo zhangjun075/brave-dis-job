d3d702ab803e95946675336942bb45ad70b58930
Jun Zhang <zhangjunheling@163.com>
2018-07-04 10:49:20 +0800

    updated by zj

--- src/main/java/com/brave/job/common/Worker.java
+++ src/main/java/com/brave/job/common/Worker.java
@@ -1,15 +1,5 @@
 package com.brave.job.common;
 
-import com.brave.config.ClientConfiguration;
-import jdk.internal.instrumentation.ClassInstrumentation;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-
 /**
  * @author junzhang
  */
